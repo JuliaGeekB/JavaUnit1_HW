b9daea6fb4925f93d956dc9a7fbddb5b7a2725dc
Юлия Викторовна <julsbi@bk.ru>
2023-06-19 02:10:37 +0300

    Task 1-3

--- HW1/HW1/src/App.java
+++ HW1/HW1/src/App.java
@@ -1,5 +1,72 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Task1. Вычислить сумму чисел от 1 до N
+        Scanner iScanner=new Scanner(System.in);
+        System.out.printf("Введите число N: ");
+        int n=iScanner.nextInt();
+        System.out.println("Сумма натуральных числе от 1 до " +n +" равняется " + Sum(n));
+                
+        // Task2.  Вывести все простые числа от 1 до 1000
+        List<Integer> primes=new ArrayList<>();
+        for (int i=2; i <=1000; i++){
+            boolean primeTest=true;
+            for (int j = 2; j < i; j++) {
+                if (i%j==0){
+                    primeTest=false;
+                    break;
+                }
+            }
+            if (primeTest){
+                primes.add(i);
+            }
+        }
+        System.out.println("Простые числа: " + primes);
+
+        // Task3.  Реализовать простой калькулятор
+        task3();
+
     }
+    private static int Sum (int n) {
+        int Sum=0;
+        for (int i = 1; i <= n; i++) {
+            Sum=Sum+i;
+        } 
+        return Sum;
+    }
+
+    public static void task3 () {
+        Scanner iScanner=new Scanner(System.in);
+        System.out.printf("Введите первое число: ");
+        int a=iScanner.nextInt();
+        System.out.printf("Введите второе число: ");
+        int b=iScanner.nextInt();
+        System.out.println("Введите операцию из списка: +, -, /, *: ");
+        char operation;
+        operation=iScanner.next().charAt(0);
+        int res;
+        switch (operation) {
+            case '+':
+                res=a+b;
+                System.out.printf("Результат: %d", res);
+                break;
+            case '-':
+                res=a-b;
+                System.out.printf("Результат: %d", res);
+                break;       
+            case '/':
+                res=a/b;
+                System.out.printf("Результат: %d", res);
+                break;
+            case '*':
+                res=a*b;
+                System.out.printf("Результат: %d", res);
+                break;
+        }
+        iScanner.close();
+    }
+
 }
